118c2ab70903dcd26ed07270088d0bdf2b5ec81f
박상현 <sa2840@naver.com>
2025-06-04 12:58:46 +0900

    cros설정 추가

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from routers.auth_router import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(auth_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://friendly-lamp-pj9wr9q5vvvghr57g-3000.app.github.dev/",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
